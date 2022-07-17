61bafc5ac5d041c329632970ddae868e69de96f6
alireza <alirezataghizadeh66@gmail.com>
2022-07-17 17:08:48 +0300

    add env and config api client for starting

--- lib/services/map_box_service.dart
+++ lib/services/map_box_service.dart
@@ -1,15 +1,18 @@
 import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+
 import 'package:spa_coding_exercise/util/array_util.dart';
 
 class MapBoxService {
-
   static String accessToken = "";
 
-  static Future<List<MapBoxPlace>> searchPlaces(String searchQuery, [int limit = 6]) async {
+  static Future<List<MapBoxPlace>> searchPlaces(String searchQuery,
+      [int limit = 6]) async {
     List<MapBoxPlace> result = [];
-    var response = await http.get("https://api.mapbox.com/geocoding/v5/mapbox.places/$searchQuery"
-        ".json?access_token=$accessToken&autocomplete=true&country=de&types=place&limit=$limit");
+    var response = await http.get(Uri.parse(
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/$searchQuery"
+        ".json?access_token=$accessToken&autocomplete=true&country=de&types=place&limit=$limit"));
 
     try {
       var decodedJson = jsonDecode(response.body);
@@ -24,26 +27,27 @@ class MapBoxService {
       print(err);
     }
 
-    return (ArrayUtil.isNotEmpty(result)) ? result : null;
+    return result;
   }
 
-  static Future<MapBoxPlace> lookup(double latitude, double longitude) async {
-    var response = await http.get("https://api.mapbox.com/geocoding/v5/mapbox.places/$longitude,$latitude"
-        ".json?access_token=$accessToken&types=region");
-
+  static Future<MapBoxPlace>? lookup(double latitude, double longitude) async {
+    var response = await http.get(Uri.parse(
+        "https://api.mapbox.com/geocoding/v5/mapbox.places/$longitude,$latitude"
+        ".json?access_token=$accessToken&types=region"));
+    var finalPlace;
     try {
       var decodedJson = jsonDecode(response.body);
       var features = decodedJson["features"];
-      if (ArrayUtil.isNotEmpty(features)) {
-        for (dynamic place in features) {
-          return MapBoxPlace.fromJson(place);
-        }
+
+      for (dynamic place in features) {
+        finalPlace = MapBoxPlace.fromJson(place);
+        break;
       }
     } catch (err) {
       print(err);
     }
 
-    return null;
+    return finalPlace;
   }
 }
 
@@ -52,12 +56,21 @@ class MapBoxPlace {
   String name;
   double latitude;
   double longitude;
+  MapBoxPlace({
+    required this.id,
+    required this.name,
+    required this.latitude,
+    required this.longitude,
+  });
 
-  MapBoxPlace.fromJson(Map<String, dynamic> json) {
-    if (json == null) return;
-    id = json['id'];
-    name = json['text'];
-    latitude = double.parse(json['center'][1].toString());
-    longitude = double.parse(json['center'][0].toString());
+  factory MapBoxPlace.fromJson(Map<String, dynamic> json) {
+    return MapBoxPlace(
+      id: json['id'],
+      name: json['text'],
+      latitude: double.parse(json['center'][1].toString()),
+      longitude: double.parse(
+        json['center'][0].toString(),
+      ),
+    );
   }
 }
